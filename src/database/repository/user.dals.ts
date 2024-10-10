91c2f2a3cfb8700bf7f50f4be38c6434a296b7e3
bruno34154 <www.brunomesenga@gmail.com>
2024-10-09 21:59:14 -0300

    back-86b2cftcv-create-login to register user

--- src/database/repository/user.dals.ts
+++ src/database/repository/user.dals.ts
@@ -26,6 +26,7 @@ class UserDALs{
 
 
 
+
 }
 
 export {UserDALs}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,50 +1,82 @@
-import {Router} from 'express';
-import {UserControllers} from '../controllers/user.controller';
+import { Router } from 'express';
+import { UserControllers } from '../controllers/user.controller';
 
 class UserRoutes {
+  private readonly router: Router;
+  private readonly userController: UserControllers;
 
-    private readonly router: Router;
-    private readonly userController: UserControllers;
+  constructor() {
+    this.router = Router();
+    this.userController = new UserControllers();
+  }
 
-    constructor() {
-        this.router = Router();
-        this.userController = new UserControllers();
-    }
-    
-    postRoutes() {
-        /** 
-    * @swagger
-    * /user/auth-user:
-    *   post:
-    *     summary: Fazer autenticação e login dos usuarios
-    *     description: Fazer autenticação e login dos usuarios
-    *     tags:
-    *       - Login User
-    *     requestBody:
-    *       required: true
-    *       content:
-    *         application/json:
-    *           schema:
-    *             type: object
-    *             properties:
-    *               email:
-    *                 type: string
-    *               password:
-    *                 type: string
-    *     responses:
-    *       '200':
-    *         description: Login feito com sucesso
-    *       401:
-    *         description: |
-    *           Erros possíveis:
-    *           - Invalid email
-    *           - Invalid password
-    *           - There is no token key
-    *           - There is no refresh token key
-    */
-        this.router.post('/auth-user', this.userController.authUser.bind(this.userController));
-         return this.router;
-     }
+  postRoutes() {
+    /** 
+     * @swagger
+     * /user/create-user:
+     *   post:
+     *     summary: Criar usuário no sistema
+     *     description: Criar usuário no sistema
+     *     tags:
+     *       - Create User
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               username:
+     *                 type: string
+     *               email:
+     *                 type: string
+     *               password:
+     *                 type: string
+     *     responses:
+     *       200:
+     *         description: Created user with sucess
+     *       400:
+     *         description: Email already exists
+     */
+    this.router.post('/create-user', this.userController.createUser.bind(this.userController));
+
+    /** 
+     * @swagger
+     * /user/auth-user:
+     *   post:
+     *     summary: Fazer autenticação e login dos usuários
+     *     description: Fazer autenticação e login dos usuários
+     *     tags:
+     *       - Login User
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               email:
+     *                 type: string
+     *               password:
+     *                 type: string
+     *     responses:
+     *       200:
+     *         description: Login feito com sucesso
+     *       404:
+     *         description: |
+     *           Erros possíveis:
+     *           - Invalid email
+     *           - Invalid password
+     *       401:
+     *         description: |
+     *           Erros possíveis:
+     *           - There is no token
+     *           - There is no refresh token
+     */
+    this.router.post('/auth-user', this.userController.authUser.bind(this.userController));
+
+    return this.router;
+  }
 }
 
-export {UserRoutes};
+export { UserRoutes };
